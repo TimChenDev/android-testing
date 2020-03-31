e298e45c5ccc24bc605947a1b63d976701524c84
Nevin Chen <cnevinchen@gmail.com>
2020-03-31 11:45:15 +0800

    Add Ktor

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/remote/TasksRemoteDataSource.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/remote/TasksRemoteDataSource.kt
@@ -23,21 +23,25 @@ import com.example.android.architecture.blueprints.todoapp.data.Result.Error
 import com.example.android.architecture.blueprints.todoapp.data.Result.Success
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
-import kotlinx.coroutines.delay
+import com.example.android.architecture.blueprints.todoapp.util.httpClientAndroid
+import com.google.gson.annotations.SerializedName
+import io.ktor.client.call.NoTransformationFoundException
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.patch
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.util.KtorExperimentalAPI
+import timber.log.Timber
 
 /**
  * Implementation of the data source that adds a latency simulating network.
  */
+@KtorExperimentalAPI
 object TasksRemoteDataSource : TasksDataSource {
 
-    private const val SERVICE_LATENCY_IN_MILLIS = 2000L
-
-    private var TASKS_SERVICE_DATA = LinkedHashMap<String, Task>(2)
-
-    init {
-        addTask("Build tower in Pisa", "Ground looks good, no foundation work required.")
-        addTask("Finish bridge in Tacoma", "Found awesome girders at half the cost!")
-    }
+    private const val SERVER_URL = "https://ktor-jib-57lqbht3qa-de.a.run.app"
 
     private val observableTasks = MutableLiveData<Result<List<Task>>>()
 
@@ -60,7 +64,7 @@ object TasksRemoteDataSource : TasksDataSource {
                 is Error -> Error(tasks.exception)
                 is Success -> {
                     val task = tasks.data.firstOrNull() { it.id == taskId }
-                        ?: return@map Error(Exception("Not found"))
+                            ?: return@map Error(Exception("Not found"))
                     Success(task)
                 }
             }
@@ -68,59 +72,81 @@ object TasksRemoteDataSource : TasksDataSource {
     }
 
     override suspend fun getTasks(): Result<List<Task>> {
-        // Simulate network by delaying the execution.
-        val tasks = TASKS_SERVICE_DATA.values.toList()
-        delay(SERVICE_LATENCY_IN_MILLIS)
+        val taskResponse =
+                httpClientAndroid.get<TaskListResponse>("$SERVER_URL/api/tasks")
+
+        val tasks = taskResponse.data.map {
+            Task(title = it.title, description = it.description, isCompleted = it.completed, id = it.id)
+        }
+
         return Success(tasks)
     }
 
     override suspend fun getTask(taskId: String): Result<Task> {
-        // Simulate network by delaying the execution.
-        delay(SERVICE_LATENCY_IN_MILLIS)
-        TASKS_SERVICE_DATA[taskId]?.let {
-            return Success(it)
-        }
-        return Error(Exception("Task not found"))
+        // NOT IMPLEMENTED
+        return Success(Task(title = "API NOT READY", description = "CAN'T GET TASK BY ID"))
     }
 
-    private fun addTask(title: String, description: String) {
-        val newTask = Task(title, description)
-        TASKS_SERVICE_DATA[newTask.id] = newTask
-    }
 
     override suspend fun saveTask(task: Task) {
-        TASKS_SERVICE_DATA[task.id] = task
+
+        try {
+            httpClientAndroid.post<Task>("$SERVER_URL/api/tasks") {
+                contentType(ContentType.Application.Json)
+                body = TaskRequest(title = task.title, description = task.description)
+            }
+        } catch (e: NoTransformationFoundException) {
+            Timber.e("Error while saveTask:${e.message}")
+        }
     }
 
     override suspend fun completeTask(task: Task) {
-        val completedTask = Task(task.title, task.description, true, task.id)
-        TASKS_SERVICE_DATA[task.id] = completedTask
+
+        httpClientAndroid.patch<Unit>("$SERVER_URL/api/tasks/${task.id}/complete")
+
     }
 
     override suspend fun completeTask(taskId: String) {
-        // Not required for the remote data source
+
+        httpClientAndroid.patch<Unit>("$SERVER_URL/api/tasks/$taskId/complete")
+
     }
 
     override suspend fun activateTask(task: Task) {
-        val activeTask = Task(task.title, task.description, false, task.id)
-        TASKS_SERVICE_DATA[task.id] = activeTask
-    }
-
-    override suspend fun activateTask(taskId: String) {
         // Not required for the remote data source
     }
 
+    override suspend fun activateTask(taskId: String) {
+        // NOT IMPLEMENTED
+    }
+
     override suspend fun clearCompletedTasks() {
-        TASKS_SERVICE_DATA = TASKS_SERVICE_DATA.filterValues {
-            !it.isCompleted
-        } as LinkedHashMap<String, Task>
+        // NOT IMPLEMENTED
     }
 
     override suspend fun deleteAllTasks() {
-        TASKS_SERVICE_DATA.clear()
+        // NOT IMPLEMENTED
     }
 
     override suspend fun deleteTask(taskId: String) {
-        TASKS_SERVICE_DATA.remove(taskId)
+
+        httpClientAndroid.delete<Unit>("$SERVER_URL/api/tasks/$taskId/delete")
+
     }
 }
+
+data class TaskRequest(
+        val title: String,
+        val description: String
+)
+
+data class TaskListResponse(
+        @SerializedName("data") val data: List<TaskResponse>
+)
+
+data class TaskResponse(
+        @SerializedName("id") val id: String,
+        @SerializedName("title") val title: String,
+        @SerializedName("description") val description: String,
+        @SerializedName("completed") val completed: Boolean
+)
